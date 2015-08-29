73246a063d23f94b74719d91f21f0ffcdc06b146
Quenio dos Santos <quenio@me.com>
2015-08-29 09:49:11 -0300

    Several tweaks

--- main.cpp
+++ main.cpp
@@ -7,26 +7,21 @@ enum PlayerMarker {
     X, O
 };
 
-inline PlayerMarker adversary(PlayerMarker playerMaker)
+inline PlayerMarker adversaryOf(const PlayerMarker & playerMaker)
 {
     return playerMaker == X ? O : X;
 }
 
-inline bool maxTurn(PlayerMarker playerMaker)
+inline bool maxTurn(const PlayerMarker & playerMaker)
 {
     return playerMaker == X;
 }
 
-inline bool minTurn(PlayerMarker playerMaker)
+class GameSlot
 {
-    return playerMaker == O;
-}
-
-class GameSlot {
-
 public:
 
-    bool isPlayerMarker(PlayerMarker playerMarker) const
+    bool hasPlayerMarker(const PlayerMarker & playerMarker) const
     {
         if (_empty)
         {
@@ -43,29 +38,23 @@ public:
         return _empty;
     }
 
-    void copyMark(const GameSlot & gameSlot)
-    {
-        this->_empty = gameSlot._empty;
-        this->_playerMarker = gameSlot._playerMarker;
-    }
+    friend class GameBoard;
+    friend bool operator == (const GameSlot & lhs, const GameSlot & rhs);
+
+private:
 
     void mark(const PlayerMarker &playerMarker)
     {
         if (_empty)
         {
-            this->_playerMarker = playerMarker;
-            this->_empty = false;
+            _playerMarker = playerMarker;
+            _empty = false;
         }
         else
         {
             throw "Game slot already marked";
         }
     }
-
-    friend bool operator == (const GameSlot & lhs, const GameSlot & rhs);
-    friend ostream & operator << (ostream &os, const GameSlot &gameSlot);
-
-private:
 
     bool _empty = true;
     PlayerMarker _playerMarker;
@@ -84,15 +73,16 @@ inline bool operator == (const GameSlot & lhs, const GameSlot & rhs)
     }
 }
 
-inline ostream & operator << (ostream &os, const GameSlot &gameSlot) {
+inline ostream & operator << (ostream &os, const GameSlot &gameSlot)
+{
     if (gameSlot.isEmpty()) {
         os << "[ ]";
     }
-    else if (gameSlot._playerMarker == PlayerMarker::X)
+    else if (gameSlot.hasPlayerMarker(PlayerMarker::X))
     {
         os << "[X]";
     }
-    else if (gameSlot._playerMarker == PlayerMarker::O)
+    else if (gameSlot.hasPlayerMarker(PlayerMarker::O))
     {
         os << "[O]";
     }
@@ -102,21 +92,23 @@ inline ostream & operator << (ostream &os, const GameSlot &gameSlot) {
 class GamePlay
 {
 public:
-    GamePlay(int line = -1, int column = -1): _line(line), _column(column) {}
 
+    GamePlay(int line = -1, int column = -1): _line { line }, _column { column }
+    {
+    }
 
     int line() const { return _line; }
     int column() const { return _column; }
-
-    bool noPlay() { return _line == -1 or _column == -1; }
 
 private:
     int _line;
     int _column;
 };
 
-class GameBoard {
+class GameBoard
+{
 public:
+
     static const int LINE_COUNT = 3;
     static const int COLUMN_COUNT = 3;
 
@@ -154,27 +146,21 @@ public:
                ((_slots[2][0] == _slots[1][1]) and (_slots[1][1] == _slots[0][2]));
     }
 
-    GameBoard mark(int newLine, int newColumn, const PlayerMarker &playerMarker) const
+    GameBoard play(GamePlay newPlay, const PlayerMarker &playerMarker) const
     {
-        if (newLine < 0 or newLine >= LINE_COUNT)
+        if (newPlay.line() < 0 or newPlay.line() >= LINE_COUNT)
         {
             throw out_of_range { "Line number out of range" };
         }
 
-        if (newColumn < 0 or newColumn >= COLUMN_COUNT)
+        if (newPlay.column() < 0 or newPlay.column() >= COLUMN_COUNT)
         {
             throw out_of_range { "Column number out of range" };
         }
 
-        GameBoard newGameBoard;
+        GameBoard newGameBoard { *this };
 
-        for (int line = 0; line < GameBoard::LINE_COUNT; line++) {
-            for (int column = 0; column < GameBoard::COLUMN_COUNT; column++) {
-                newGameBoard._slots[line][column].copyMark(_slots[line][column]);
-            }
-        }
-
-        newGameBoard._slots[newLine][newColumn].mark(playerMarker);
+        newGameBoard._slots[newPlay.line()][newPlay.column()].mark(playerMarker);
 
         return newGameBoard;
     }
@@ -185,7 +171,7 @@ public:
 
         for (int line = 0; line < GameBoard::LINE_COUNT; line++) {
             for (int column = 0; column < GameBoard::COLUMN_COUNT; column++) {
-                if (! _slots[line][column].isEmpty())
+                if (_slots[line][column].isEmpty())
                 {
                     legalPlays.push_back(GamePlay(line, column));
                 }
@@ -228,9 +214,14 @@ public:
         return 0;
     }
 
-    int utilityScoreOfSlot(int line ,int column) const
+    int utilityScoreOfSlot(int line, int column) const
     {
-        if (_slots[line][column].isPlayerMarker(X))
+        if (_slots[line][column].isEmpty())
+        {
+            throw runtime_error { "There is no utility score for empty slot." };
+        }
+
+        if (_slots[line][column].hasPlayerMarker(X))
         {
             return +1;
         }
@@ -249,7 +240,9 @@ public:
     friend ostream & operator << (ostream &os, const GameBoard &gameBoard);
 
 private:
+
     GameSlot _slots[LINE_COUNT][COLUMN_COUNT];
+
 };
 
 ostream & operator << (ostream &os, const GameBoard &gameBoard) {
@@ -265,6 +258,7 @@ ostream & operator << (ostream &os, const GameBoard &gameBoard) {
 class Player
 {
 public:
+
     Player(PlayerMarker marker) : _marker(marker) { }
     virtual ~Player() {}
 
@@ -273,7 +267,9 @@ public:
     friend bool operator == (Player & lhs, Player & rhs);
 
 protected:
+
     const PlayerMarker _marker;
+
 };
 
 bool operator == (Player & lhs, Player & rhs)
@@ -284,16 +280,22 @@ bool operator == (Player & lhs, Player & rhs)
 class GameNode
 {
 public:
-    GameNode(GameBoard gameBoard): _gameBoard(gameBoard) {}
-    GameNode(GamePlay gamePlay, GameBoard gameBoard): _gamePlay(gamePlay), _gameBoard(gameBoard) {}
 
-    vector<GameNode> childrenFor(PlayerMarker playerMarker)
+    GameNode(const GameBoard & gameBoard): _gameBoard { gameBoard }
+    {
+    }
+
+    GameNode(const GamePlay & gamePlay, const GameBoard & gameBoard): _gamePlay { gamePlay }, _gameBoard { gameBoard }
+    {
+    }
+
+    vector<GameNode> childrenFor(const PlayerMarker & playerMarker)
     {
         vector<GameNode> result;
-        for (const auto & play : _gameBoard.allLegalPlays())
+        for (const auto &newPlay : _gameBoard.allLegalPlays())
         {
-            GameBoard gameBoard = _gameBoard.mark(play.line(), play.column(), playerMarker);
-            result.push_back(GameNode(play, gameBoard));
+            GameBoard newGameBoard { _gameBoard.play(newPlay, playerMarker) };
+            result.push_back(GameNode { newPlay, newGameBoard });
         }
         return result;
     }
@@ -303,22 +305,24 @@ public:
     int score() const { return _gameBoard.score(); }
 
 private:
+
     const GamePlay _gamePlay;
     const GameBoard _gameBoard;
+
 };
 
 class GameTree {
 public:
 
-    GameTree(GameBoard currentBoard): _root(GameNode(currentBoard)) {}
+    GameTree(GameBoard currentBoard): _root { GameNode { currentBoard }  } {}
 
-    GamePlay bestPlayFor(PlayerMarker playerMarker)
+    GamePlay bestPlayFor(const PlayerMarker & playerMarker)
     {
         int maxScore = MIN_SCORE;
         GamePlay bestPlay(-1, -1);
         for (const auto &gameNode : _root.childrenFor(playerMarker))
         {
-            int score = minMax(gameNode, adversary(playerMarker));
+            int score = minMax(gameNode, adversaryOf(playerMarker));
             if (score > maxScore)
             {
                 maxScore = score;
@@ -343,11 +347,11 @@ private:
         }
         else if (maxTurn(playerMarker))
         {
-            return max(children, adversary(playerMarker));
+            return max(children, adversaryOf(playerMarker));
         }
         else
         {
-            return min(children, adversary(playerMarker));
+            return min(children, adversaryOf(playerMarker));
         }
     }
 
@@ -380,18 +384,21 @@ private:
     }
 
     GameNode _root;
+
 };
 
 class AIPlayer: public Player
 {
 public:
-    AIPlayer(const PlayerMarker &mark) : Player(mark) { }
+    AIPlayer(): Player { X }
+    {
+    }
 
     GameBoard play(GameBoard& gameBoard)
     {
         GameTree gameTree(gameBoard);
-        GamePlay play = gameTree.bestPlayFor(_marker);
-        return gameBoard.mark(play.line(), play.column(), _marker);
+        GamePlay bestPlay = gameTree.bestPlayFor(_marker);
+        return gameBoard.play(bestPlay, _marker);
     }
 
 };
@@ -399,7 +406,9 @@ public:
 class HumanPlayer: public Player
 {
 public:
-    HumanPlayer(const PlayerMarker &mark) : Player(mark) { }
+    HumanPlayer(): Player { O }
+    {
+    }
 
     GameBoard play(GameBoard &gameBoard)
     {
@@ -415,20 +424,21 @@ public:
 
         cout << endl;
 
-        return gameBoard.mark(line, column, _marker);
+        return gameBoard.play(GamePlay { line, column }, _marker);
     }
 
 };
 
-int main() {
-    shared_ptr<Player> playerX { new AIPlayer(X) };
-    shared_ptr<Player> playerO { new HumanPlayer(O) };
+int main()
+{
+    shared_ptr<Player> ai { new AIPlayer() };
+    shared_ptr<Player> human { new HumanPlayer() };
     GameBoard initialBoard;
 
     cout << "Gamed Started!" << endl << endl << initialBoard << endl;
 
     int playCount = 0;
-    shared_ptr<Player> currentPlayer = playerX;
+    shared_ptr<Player> currentPlayer = human;
     GameBoard currentBoard = initialBoard;
 
     while (!currentBoard.isGameOver())
@@ -437,14 +447,7 @@ int main() {
 
         cout << "Play No.: " << ++playCount << endl << endl << currentBoard << endl;
 
-        if (*currentPlayer == *playerX)
-        {
-            currentPlayer = playerO;
-        }
-        else
-        {
-            currentPlayer = playerX;
-        }
+        currentPlayer = (currentPlayer == ai ? human : ai);
     }
 
     return 0;
